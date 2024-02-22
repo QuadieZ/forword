8d8ed68e481652c9817cf1ba6588b3f1663c54f5
quadiez <dduidokdoi@gmail.com>
2024-02-22 16:13:23 +0700

    feat: setup supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import supabase from "@/supabase";
 import Image from "next/image";
 
 export default function Home() {
+  console.log(supabase);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
