d8c482c5c2e12c7460d6af9946bdde4f7e41bc7c
Yapar Winwan <130440272+whyapar@users.noreply.github.com>
2024-02-28 12:15:15 +0700

    loginWithEmail.ts

---
+++ src/supabase/utils/auth/loginWithEmail.ts
@@ -0,0 +1,9 @@
+import supabase from "@/supabase";
+
+export async function signInWithEmail() {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: 'example@email.com',
+      password: 'example-password',
+    })
+  }
+  
